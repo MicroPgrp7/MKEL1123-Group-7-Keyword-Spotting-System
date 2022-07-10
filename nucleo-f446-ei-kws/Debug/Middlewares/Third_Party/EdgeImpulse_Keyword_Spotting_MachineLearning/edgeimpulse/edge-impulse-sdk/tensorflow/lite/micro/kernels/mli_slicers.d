4319fbf689ebd2ea5c1d39ae205242ae6f36c6d5
MicroPgrp7 <105091269+MicroPgrp7@users.noreply.github.com>
2022-07-11 01:47:56 +0800

    Add files via upload

---
+++ nucleo-f446-ei-kws/Debug/Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/mli_slicers.d
@@ -0,0 +1,4 @@
+Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/mli_slicers.o: \
+ ../Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/mli_slicers.cpp \
+ ../Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/../../../../classifier/ei_classifier_config.h
+../Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/../../../../classifier/ei_classifier_config.h:
---
+++ nucleo-f446-ei-kws/Debug/Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.d
@@ -0,0 +1,4 @@
+Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.o: \
+ ../Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.cpp \
+ ../Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/../../../../classifier/ei_classifier_config.h
+../Middlewares/Third_Party/EdgeImpulse_Keyword_Spotting_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/../../../../classifier/ei_classifier_config.h:
